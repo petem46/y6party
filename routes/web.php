e73042577697be40f041b6a253357a30362a830a
Pete <petem46@gmail.com>
2019-02-25 18:54:08 +0000

    middleware

--- routes/web.php
+++ routes/web.php
@@ -15,32 +15,38 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
+Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
+{
+    Route::resource('settings', 'SettingsController');    
+});
+
+Route::group(['middleware' => 'App\Http\Middleware\ParentMiddleware'], function()
+{
+    Route::get('/iamjob', 'JobsController@index')->name('iamjob'); 
+    Route::get('/addjob', 'JobsController@create')->name('addjob');
+    Route::post('/jobs/{job}/comments', 'CommentsController@store');
+    Route::resource('jobs', 'JobsController');    
+    Route::resource('comments', 'CommentsController');    
+});
+
 // Protected Routes
 Route::group(['middleware' => 'auth'], function () {
     // Registration Routes...
-    // Route::get('auth/register', 'Auth\AuthController@getRegister');
-    // Route::post('auth/register', 'Auth\AuthController@postRegister');
+    Route::get('auth/register', 'Auth\AuthController@getRegister');
+    Route::post('auth/register', 'Auth\AuthController@postRegister');
     
     // Named routes...
     // Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/profile', 'ProfileController@show')->name('profile'); 
     Route::get('/playlist', 'SongsController@index')->name('playlist'); 
-    Route::get('/iamjob', 'JobsController@index')->name('iamjob'); 
     Route::get('/party', 'HomeController@index')->name('party');
     // Route::get('/theplaylist', 'SongsController@index')->name('theplaylist'); 
     // Route::get('/thejobs', 'JobsController@index')->name('thejobs'); 
     // Route::get('/theparty', 'JobsController@index')->name('theparty'); 
     // Add new routes
     Route::get('/addsong', 'SongsController@create')->name('addsong');
-    Route::get('/addjob', 'JobsController@create')->name('addjob');
-
-    Route::post('/jobs/{job}/comments', 'CommentsController@store');
-
     // Resource Routes...
     Route::resource('songs', 'SongsController');
-    Route::resource('jobs', 'JobsController');    
-    Route::resource('comments', 'CommentsController');    
-    Route::resource('settings', 'SettingsController');    
 });
     
 // Home Routes

5ac0648ac955c76737c26e5c37e25e11fc0ed6d6
Pete <petem46@gmail.com>
2019-03-09 12:56:36 +0000

    sub domain routing for demo

--- routes/web.php
+++ routes/web.php
@@ -15,47 +15,54 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-Route::domain('demo.y6party.com')->group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
+Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
 {
-    Route::resource('settings', 'SettingsController');    
-    Route::get('/iamjob', 'JobsController@index')->name('iamjob'); 
-    Route::get('/addjob', 'JobsController@create')->name('addjob');
-    Route::post('/jobs/{job}/comments', 'CommentsController@store');
-    Route::resource('jobs', 'JobsController');    
-    Route::post('/jobs/{job}/updatejob', 'JobsController@updatejob');
-    Route::resource('comments', 'CommentsController');    
+    Route::group(['domain' => 'demo.y6party.com'], function () 
+    {
+        Route::resource('settings', 'SettingsController');    
+        Route::get('/iamjob', 'JobsController@index')->name('iamjob'); 
+        Route::get('/addjob', 'JobsController@create')->name('addjob');
+        Route::post('/jobs/{job}/comments', 'CommentsController@store');
+        Route::resource('jobs', 'JobsController');    
+        Route::post('/jobs/{job}/updatejob', 'JobsController@updatejob');
+        Route::resource('comments', 'CommentsController');  
+    }  
 });
 
-Route::domain('demo.y6party.com')->group(['middleware' => 'App\Http\Middleware\ParentMiddleware'], function()
+Route::group(['middleware' => 'App\Http\Middleware\ParentMiddleware'], function()
 {
-    Route::get('/iamjob', 'JobsController@index')->name('iamjob'); 
-    Route::get('/addjob', 'JobsController@create')->name('addjob');
-    Route::post('/jobs/{job}/comments', 'CommentsController@store');
-    Route::resource('jobs', 'JobsController');    
-    Route::post('/jobs/{job}/updatejob', 'JobsController@updatejob');
-    Route::resource('comments', 'CommentsController');    
-    Route::get('/profile', 'ProfileController@show')->name('profile'); 
-    Route::get('/playlist', 'SongsController@index')->name('playlist'); 
-    Route::get('/party', 'HomeController@index')->name('party');
-
+    Route::group(['domain' => 'demo.y6party.com'], function () 
+    {
+        Route::get('/iamjob', 'JobsController@index')->name('iamjob'); 
+        Route::get('/addjob', 'JobsController@create')->name('addjob');
+        Route::post('/jobs/{job}/comments', 'CommentsController@store');
+        Route::resource('jobs', 'JobsController');    
+        Route::post('/jobs/{job}/updatejob', 'JobsController@updatejob');
+        Route::resource('comments', 'CommentsController');    
+        Route::get('/profile', 'ProfileController@show')->name('profile'); 
+        Route::get('/playlist', 'SongsController@index')->name('playlist'); 
+        Route::get('/party', 'HomeController@index')->name('party');
+    }
 });
 
-Route::domain('demo.y6party.com')->group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function () {
 
-    Route::get('auth/register', 'Auth\AuthController@getRegister');
-    Route::post('auth/register', 'Auth\AuthController@postRegister');
-    
-    Route::get('/profile', 'ProfileController@show')->name('profile'); 
-    Route::get('/playlist', 'SongsController@index')->name('playlist'); 
-    Route::get('/party', 'HomeController@index')->name('party');
-    Route::post('/songsearch', 'SongsController@songsearch')->name('songsearch');
+    Route::group(['domain' => 'demo.y6party.com'], function () 
+    {
+        Route::get('auth/register', 'Auth\AuthController@getRegister');
+        Route::post('auth/register', 'Auth\AuthController@postRegister');
+        
+        Route::get('/profile', 'ProfileController@show')->name('profile'); 
+        Route::get('/playlist', 'SongsController@index')->name('playlist'); 
+        Route::get('/party', 'HomeController@index')->name('party');
+        Route::post('/songsearch', 'SongsController@songsearch')->name('songsearch');
 
-    Route::get('/addsong', 'SongsController@create')->name('addsong');
-    Route::get('/songs/search', 'SongsController@search');
-    Route::get('/songs/search/{search}', 'SongsController@songsearch');
-    
-    Route::resource('songs', 'SongsController');
-
+        Route::get('/addsong', 'SongsController@create')->name('addsong');
+        Route::get('/songs/search', 'SongsController@search');
+        Route::get('/songs/search/{search}', 'SongsController@songsearch');
+        
+        Route::resource('songs', 'SongsController');
+    }
 });
     
 // Home Routes
